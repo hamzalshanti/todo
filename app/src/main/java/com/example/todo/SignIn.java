73afc04706a92f9e0cab8c2207babfb18365d9fc
Hamza Alshanti <hamza2000pal@gmail.com>
2020-12-04 22:04:59 +0200

    run next button

---
+++ app/src/main/java/com/example/todo/SignIn.java
@@ -0,0 +1,13 @@
+package com.example.todo;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignIn extends AppCompatActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sing_in);
+    }
+}
+
